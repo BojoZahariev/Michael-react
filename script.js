6ef2e9cf28cf345a0962555ad411060e65d8dc52
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-27 09:49:08 +0100

    offset

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const Trailer = props => {
-  return <div>Trailer</div>;
+const Truck = props => {
+  return <div>Truck</div>;
 };
+
+const Bin = props => {
+  return (
+    <div className='bin' id='test'>
+      Bin
+    </div>
+  );
+};
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  getOffset = () => {
+    console.log(this.myRef.current.offsetTop);
+    console.log(this.myRef.current.offsetLeft);
+  };
+  render() {
+    return (
+      <div ref={this.myRef} onClick={this.getOffset}>
+        click
+      </div>
+    );
+  }
+}
 
 class Container extends React.Component {
   constructor(props) {
@@ -14,7 +41,9 @@ class Container extends React.Component {
   render() {
     return (
       <div>
-        <Trailer />
+        <Truck />
+        <Bin />
+        <MyComponent />
       </div>
     );
   }
@@ -22,3 +51,12 @@ class Container extends React.Component {
 
 const domContainer = document.querySelector('#root');
 ReactDOM.render(<Container />, domContainer);
+
+/*
+var el = document.querySelector('#test');
+var viewportOffset = el.getBoundingClientRect();
+// these are relative to the viewport, i.e. the window
+var top = viewportOffset.top;
+var left = viewportOffset.left;
+console.log(top, left);
+*/
